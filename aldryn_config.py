34732711cb6cb54c7fce7e6a5016f77c2b9d2dfa
Christian Bertschy <christian.bertschy@divio.ch>
2014-07-20 20:22:33 +0200

    tried fix

--- aldryn_config.py
+++ aldryn_config.py
@@ -2,8 +2,8 @@
 from aldryn_client import forms
 
 class Form(forms.BaseForm):
-    cms_cascade_plugins = forms.CharField('List of Cascade Plugins (comma separated)', required=False)
+    cms_cascade_plugins = forms.CharField('Cascade Plugins', required=False)
 
     def to_settings(self, data, settings):
-        settings['CMS_CASCADE_PLUGINS'] = data.get('cms_cascade_plugins')
+        settings['CMS_CASCADE_PLUGINS'] = data.get('cms_cascade_plugins', '')
         return settings
