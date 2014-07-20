abc60463e98e7a0a74ecace922ed545b0dc88c84
Christian Bertschy <christian.bertschy@divio.ch>
2014-07-20 19:19:58 +0200

    added form for cascade plugins

--- aldryn_config.py
+++ aldryn_config.py
@@ -2,6 +2,8 @@
 from aldryn_client import forms
 
 class Form(forms.BaseForm):
+    cms_cascade_plugins = forms.CharField('List of Cascade Plugins (comma separated)', required=False)
+
     def to_settings(self, data, settings):
-        settings['CMS_CASCADE_PLUGINS'] = data.get('cms_cascade_plugins', '')
+        settings['CMS_CASCADE_PLUGINS'] = data.get('cms_cascade_plugins')
         return settings
