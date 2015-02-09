729fc10ba892582835c0c60a78839ff87e421e24
Christian Bertschy <christian.bertschy@divio.ch>
2015-02-09 13:26:04 +0100

    Fixed list

--- aldryn_config.py
+++ aldryn_config.py
@@ -5,5 +5,5 @@ class Form(forms.BaseForm):
     cms_cascade_plugins = forms.CharField('Cascade Plugins', required=False)
 
     def to_settings(self, data, settings):
-        settings['CMS_CASCADE_PLUGINS'] = data.get('cms_cascade_plugins', '')
+        settings['CMS_CASCADE_PLUGINS'] = [x.strip() for x in data.get('cms_cascade_plugins', '').split(",")]
         return settings
